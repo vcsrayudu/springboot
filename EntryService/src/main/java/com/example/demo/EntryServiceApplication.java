9aef5d597120763e21d253cb88d54bcd9b2f811b
Chinna Subbarayudu Vidudala <cvidudal@opentext.com>
2019-05-06 16:14:18 +0530

    initial commit

--- EntryService/src/main/java/com/example/demo/EntryServiceApplication.java
+++ EntryService/src/main/java/com/example/demo/EntryServiceApplication.java
@@ -3,6 +3,8 @@ package com.example.demo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
@@ -13,6 +15,7 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 @RestController
 @RequestMapping("/entry")
+@EnableDiscoveryClient
 public class EntryServiceApplication {
 	@Autowired
 	private RestTemplate restTemplate;
@@ -31,6 +34,14 @@ public class EntryServiceApplication {
 		String responce =restTemplate.exchange("http://localhost:8081/app/welcome/{calledFrom}", HttpMethod.GET, null, new ParameterizedTypeReference<String>() {}, calledFrom).getBody();
 		return responce;
 	}
+	@RequestMapping("/welcome2")
+	public String welcome2()
+	{
+		String calledFrom="Entry Service2";
+		String responce =restTemplate.exchange("http://microstarter/app/welcome/{calledFrom}", HttpMethod.GET, null, new ParameterizedTypeReference<String>() {}, calledFrom).getBody();
+		return responce;
+	}
+	@LoadBalanced
 	@Bean
 	public RestTemplate restTemplate()
 	{
